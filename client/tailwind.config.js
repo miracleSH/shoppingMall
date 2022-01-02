c7bff60f463aba4ab11a90ef95372f0e9536a91f
SH <https://github.com/miracleSH>
2022-01-02 20:22:23 +0900

    tilewind css 의존성 추가 및 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +8,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
 };
