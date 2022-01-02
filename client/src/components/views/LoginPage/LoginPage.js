fd05bdcacc68da5de3bbdca30a49d25675878bf8
SH <https://github.com/miracleSH>
2022-01-02 23:48:55 +0900

    UI 수정 및 tailwind 플러그인 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -39,15 +39,9 @@ const LoginPage = (props) => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{" "}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
-            </a>
-          </p>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign In</h2>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onSubmit={onSubmitHandler}>
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
@@ -62,6 +56,8 @@ const LoginPage = (props) => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
+                value={email}
+                onChange={onEmailHandler}
               />
             </div>
             <div>
@@ -76,6 +72,8 @@ const LoginPage = (props) => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                value={password}
+                onChange={onPasswordHandler}
               />
             </div>
           </div>
--- client/src/components/views/Navbar/Navbar.js
+++ client/src/components/views/Navbar/Navbar.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
-import { useDispatch, useStore } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { auth } from "../../../_actions/user_action";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function Navbar(props) {
   const navigate = useNavigate();
@@ -32,7 +35,7 @@ function Navbar(props) {
   /**
    * 로그아웃 클릭 메소드
    */
-  const onClickHandler = () => {
+  const logoutHandler = () => {
     axios.get("/api/users/logout").then((_res) => {
       if (_res.data.success) {
         setUserName("");
@@ -41,34 +44,171 @@ function Navbar(props) {
     });
   };
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  const navigation = [
+    { name: "회원가입", href: "/register", current: false },
+    { name: "상품등록", href: "/product/upload", current: false },
+    { name: "Login", href: "/login", current: false },
+    { name: "Logout", href: "/logout", current: false },
+  ];
+
+  const navigationMenuRender = () => {
+    return navigation.map((item) => {
+      if (item.name === "Logout") {
+        return (
+          <button
+            key={item.name}
+            onClick={logoutHandler}
+            className={classNames(
+              item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+              "px-3 py-2 rounded-md text-sm font-medium"
+            )}
+            aria-current={item.current ? "page" : undefined}
+          >
+            {item.name}
+          </button>
+        );
+      } else if (item.name === "Login") {
+        if (!isLogin) {
+          return (
+            <Link
+              key={item.name}
+              to={item.href}
+              className={classNames(
+                item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "px-3 py-2 rounded-md text-sm font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              {item.name}
+            </Link>
+          );
+        }
+      } else {
+        return (
+          <Link
+            key={item.name}
+            to={item.href}
+            className={classNames(
+              item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+              "px-3 py-2 rounded-md text-sm font-medium"
+            )}
+            aria-current={item.current ? "page" : undefined}
+          >
+            {item.name}
+          </Link>
+        );
+      }
+    });
+  };
+
   /**
    * 랜더링되는 부분
    */
   return (
-    <div className="h-10  inset-x-0 top-0 bg-black">
-      <div className="container content-center mx-auto">
-        <ul className="">
-          <li className="content-center">
-            <button className="items-center float-right font-bold text-white" onClick={onClickHandler}>
-              로그아웃
-            </button>
-          </li>
-          <li className="items-center content-center mr-10 float-right font-bold text-white">
-            {isLogin ? <div>{userName}</div> : <Link to={"/login"}>로그인</Link>}
-          </li>
-          <li>
-            <Link className="items-center mr-10 float-right" to={"/product/upload"}>
-              <div className="font-bold text-white">상품등록</div>
-            </Link>
-          </li>
-          <li>
-            <Link className="items-center mr-10 float-right" to={"/register"}>
-              <div className="font-bold text-white">회원가입</div>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <Link to="/">
+                    <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                    <img
+                      className="hidden lg:block h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                      alt="Workflow"
+                    />
+                  </Link>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">{navigationMenuRender()}</div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  <div>
+                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a href="#" className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}>
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a href="#" className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}>
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
 
